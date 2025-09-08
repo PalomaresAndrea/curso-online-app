56ea6d92369a681587939fe54e2b8a17442166ee
Andrea Palomares <chos062003@gmail.com>
2025-09-08 00:58:34 -0600

    cambio

--- src/paginas/Home.jsx
+++ src/paginas/Home.jsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 export default function Home() {
   return (
     <div className="card">
-      <h1>Bienvenida/o 👋</h1>
-      <p className="muted">SPA con React. Gestiona cursos (CRUD) desde el navegador usando localStorage.</p>
+      <h1>Bienvenida/o a Cursos Online👋</h1>
+      <p className="muted">Hola bien benidos a los cursos de programación con los mejores maestros</p>
       <div className="spacer" />
       <Link className="btn" to="/cursos">Ir a Cursos</Link>
     </div>
