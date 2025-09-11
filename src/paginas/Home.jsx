501bfbcb338498874e167b43519e5158c70632ee
Andrea Palomares <chos062003@gmail.com>
2025-09-11 13:51:54 -0600

    cambio

--- src/paginas/Home.jsx
+++ src/paginas/Home.jsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 export default function Home() {
   return (
     <div className="card">
-      <h1>Bienvenida/o a Cursos Online👋</h1>
-      <p className="muted">Hola bien benidos a los cursos de programación con los mejores maestros</p>
+      <h1>Bienvenida/o a Cursos Online</h1>
+      <p className="muted">Hola bienvenidos a los cursos de programación con los mejores maestros</p>
       <div className="spacer" />
       <Link className="btn" to="/cursos">Ir a Cursos</Link>
     </div>
